336d52c20c123f2d362aa195c2efc538e04453ff
Try Ajitiono <ballinst@gmail.com>
2026-01-24 11:23:54 +0000

    reset

--- packages/server/src/app.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MatchModule } from './match/match.module';
-
-@Module({
-  imports: [MatchModule],
-})
-export class AppModule {}
--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,15 +1,8 @@
 import 'reflect-metadata';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  // enable CORS so Vite dev server (client) can call this API during development
-  app.enableCors({ origin: 'http://localhost:5173' });
-  const port = process.env.PORT ? Number(process.env.PORT) : 4000;
-  await app.listen(port);
-  // eslint-disable-next-line no-console
-  console.log(`Nest server listening on ${port}`);
+  // Server entry point
+  console.log('Server bootstrapping...');
 }
 
 bootstrap();
