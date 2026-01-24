a9f66b1e27e723d4a356f094fbefd39de7f5c84c
Try Ajitiono <ballinst@gmail.com>
2026-01-24 15:11:18 +0000

    unfortunately no github pages

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || '/',
   plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
